2ce8e00e2e6509d26daacd2029a96daadb990eba
simongarisch <gatman946@gmail.com>
2019-08-27 06:21:46 +1000

    initial setup

---
+++ setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+
+with open("README.md") as readme_file:
+    readme = readme_file.read()
+
+setup(
+    author="Simon Garisch",
+    author_email="gatman946@gmail.com",
+    description="A typing tutor built with PyQt.",
+    install_requires=["PyQt5>=5.13.0"],
+    license="MIT license",
+    long_description=readme,
+    include_package_data=True,
+    keywords="pytypist",
+    name="pytypist",
+    packages=find_packages(include=["pytypist"]),
+    test_suite="tests",
+    url="https://github.com/simongarisch/pytypist",
+    version="0.1.0",
+)
