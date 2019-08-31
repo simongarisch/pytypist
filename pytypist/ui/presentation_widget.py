d9754594ea8c2b99185af6ff64768c91df9b605c
simongarisch <gatman946@gmail.com>
2019-08-31 18:16:02 +1000

    adding basic lesson content

--- pytypist/ui/presentation_widget.py
+++ pytypist/ui/presentation_widget.py
@@ -2,19 +2,19 @@ import os
 from PyQt5 import QtCore, QtGui, QtWidgets, QtWebEngineWidgets
 
 
+def get_presentation_index(name="example"):
+    dire_path = os.path.dirname(os.path.realpath(__file__))
+    file_path = os.path.join(
+        dire_path, "presentations", name, "index.html"
+    )
+    return file_path
+
+
 class PresentationWidget(QtWidgets.QWidget):
     def __init__(self):
         super().__init__()
         self.browser = QtWebEngineWidgets.QWebEngineView()
         self.setCentralWidget(self.browser)
-    
-    @staticmethod
-    def get_presentation_index(name="example"):
-        dire_path = os.path.dirname(os.path.realpath(__file__))
-        file_path = os.path.join(
-            dire_path, "presentations", name, "index.html"
-        )
-        return file_path
 
     def load(self, name):
         index_path = get_presentation_index(name)
