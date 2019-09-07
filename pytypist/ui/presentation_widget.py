43e9bc5e6a96031b6e683ff4173587a93417ddc5
simongarisch <gatman946@gmail.com>
2019-09-08 07:10:22 +1000

    modifying search for index.html

--- pytypist/ui/presentation_widget.py
+++ pytypist/ui/presentation_widget.py
@@ -2,12 +2,32 @@ import os
 from PyQt5 import QtCore, QtGui, QtWidgets, QtWebEngineWidgets
 
 
+class PresentationError(Exception):
+    """ Base exception relating to presentations. """
+
+
+class PresentationNotFound(PresentationError):
+    """ Unable to find index.html. """
+
+
 def get_presentation_index(name="example"):
-    dire_path = os.path.dirname(os.path.realpath(__file__))
-    file_path = os.path.join(
-        dire_path, "presentations", name, "index.html"
+    dire_path = os.path.join(
+        os.path.dirname(os.path.realpath(__file__)),
+        "presentations",
+        name
     )
-    return file_path
+
+    # check for index.html in the root directory
+    file_path = os.path.join(dire_path, "index.html")
+    if os.path.isfile(file_path):
+        return file_path
+
+    # also check in 'site_folder'
+    file_path = os.path.join(dire_path, "site_folder", "index.html")
+    if os.path.isfile(file_path):
+        return file_path
+
+    raise PresentationNotFound("Cannot find index.html.")
 
 
 class PresentationWidget(QtWidgets.QWidget):
