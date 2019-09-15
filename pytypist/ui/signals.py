9c06f10afead4410c3fbef3e1287c1c5547c3563
simongarisch <gatman946@gmail.com>
2019-09-16 06:45:48 +1000

    adding typing time signal

--- pytypist/ui/signals.py
+++ pytypist/ui/signals.py
@@ -9,6 +9,7 @@ class Signals(QtCore.QObject):
 
     start_countdown = QtCore.pyqtSignal()
     update_countdown = QtCore.pyqtSignal(int)
+    update_typing_time = QtCore.pyqtSignal(int)
     enable_typing = QtCore.pyqtSignal()
     disable_typing = QtCore.pyqtSignal()
 
